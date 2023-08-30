bb0cf558ec71011c926ec37112896a9b181c3a4a
ivanh3486 <ivanh3486@gmail.com>
2023-08-29 18:56:55 -0700

    adding hello world

--- src/main/java/org/example/hello/HelloWorld.java
+++ src/main/java/org/example/hello/HelloWorld.java
@@ -2,6 +2,6 @@ package org.example.hello;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world1");
     }
 }
